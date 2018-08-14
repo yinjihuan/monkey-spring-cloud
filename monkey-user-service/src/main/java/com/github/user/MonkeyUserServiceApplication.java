194a7426dc0ea692e774edc92ad6356a9d02d700
尹吉欢 <yinjihuan@fangjia.com>
2018-08-14 15:45:21 +0800

    提交monkey-user-service框架

---
+++ monkey-user-service/src/main/java/com/github/user/MonkeyUserServiceApplication.java
@@ -0,0 +1,20 @@
+package com.github.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * 用户服务启动入口
+ * 
+ * @author yinjihuan
+ * 
+ * @about http://cxytiandi.com/about
+ * 
+ * @date 2018-08-14
+ * 
+ */
+@SpringBootApplication
+public class MonkeyUserServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MonkeyUserServiceApplication.class, args);
+	}
+}
