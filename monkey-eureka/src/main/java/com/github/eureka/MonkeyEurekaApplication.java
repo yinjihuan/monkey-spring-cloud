a126887b6597a21567f27b4d2eb4a6dce871f091
尹吉欢 <yinjihuan@fangjia.com>
2018-08-14 15:37:36 +0800

    修改注释

--- monkey-eureka/src/main/java/com/github/eureka/MonkeyEurekaApplication.java
+++ monkey-eureka/src/main/java/com/github/eureka/MonkeyEurekaApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 /**
- * 网关启动入口
+ * 注册中心启动入口
  * 
  * @author yinjihuan
  * 
