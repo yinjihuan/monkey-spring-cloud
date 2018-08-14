f3b0d9563fe3304d83793b6a79f052b2c0f37f8c
尹吉欢 <yinjihuan@fangjia.com>
2018-08-14 17:45:09 +0800

    提交通用的返回格式类

---
+++ monkey-common/src/main/java/com/github/common/base/Response.java
@@ -0,0 +1,40 @@
+package com.github.common.base;
+/**
+ * REST API 返回数据工具类
+ * 
+ * @author yinjihuan
+ * 
+ * @about http://cxytiandi.com/about
+ * 
+ * @date 2018-08-14
+ * 
+ */
+public class Response {
+
+	private Response() {}
+	
+	public static <E> ResponseData<E> ok(E data) {
+		ResponseData<E> res = new ResponseData<E>();
+		res.ok(data);
+		return res;
+	}
+	
+	public static <E> ResponseData<E> ok(E data, String message) {
+		ResponseData<E> res = new ResponseData<E>();
+		res.ok(data, message);
+		return res;
+	}
+	
+	public static <E> ResponseData<E> fail(String message) {
+		ResponseData<E> res = new ResponseData<E>();
+		res.fail(message);
+		return res;
+	}
+	
+	public static <E> ResponseData<E> fail(String message, ResponseCode code) {
+		ResponseData<E> res = new ResponseData<E>();
+		res.fail(message, code);
+		return res;
+	}
+	
+}
