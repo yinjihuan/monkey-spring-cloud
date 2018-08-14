e988415bf8474153609ec25789f1eae557759d7f
尹吉欢 <yinjihuan@fangjia.com>
2018-08-14 15:20:32 +0800

    提交monkey-gateway框架

---
+++ monkey-gateway/src/main/java/com/github/gateway/MonkeyGatewayApplication.java
@@ -0,0 +1,20 @@
+package com.github.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * 网关启动入口
+ * 
+ * @author yinjihuan
+ * 
+ * @about http://cxytiandi.com/about
+ * 
+ * @date 2018-08-14
+ * 
+ */
+@SpringBootApplication
+public class MonkeyGatewayApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MonkeyGatewayApplication.class, args);
+	}
+}
