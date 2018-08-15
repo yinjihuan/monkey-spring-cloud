eaa4609f98f90726191a625eca3cd8e4bc69532e
尹吉欢 <yinjihuan@fangjia.com>
2018-08-15 11:28:41 +0800

    增加数据库配置和依赖

---
+++ monkey-user-service/src/main/java/com/github/user/config/BeanConfig.java
@@ -0,0 +1,20 @@
+package com.github.user.config;
+
+import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.cxytiandi.jdbc.CxytiandiJdbcTemplate;
+
+@Configuration
+public class BeanConfig {
+	
+	/**
+	 * JDBC
+	 * @return
+	 */
+	@Bean(autowire=Autowire.BY_NAME)
+	public CxytiandiJdbcTemplate cxytiandiJdbcTemplate() {
+		return new CxytiandiJdbcTemplate("com.github.user.po");
+	}
+	
+}
