38f13feee7194cba807cef073976d5b677ccaa87
尹吉欢 <yinjihuan@fangjia.com>
2018-08-15 17:28:23 +0800

    提交monkey-article-service框架

---
+++ monkey-article-service/src/main/java/com/github/article/MonkeyArticleServiceApplication.java
@@ -0,0 +1,22 @@
+package com.github.article;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * 文章服务启动入口
+ * 
+ * @author yinjihuan
+ * 
+ * @about http://cxytiandi.com/about
+ * 
+ * @date 2018-08-15
+ * 
+ */
+@SpringBootApplication
+public class MonkeyArticleServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MonkeyArticleServiceApplication.class, args);
+	}
+}
