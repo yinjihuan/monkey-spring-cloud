d41b2bb2a488822183b5ec26017a20a57ebd6da8
尹吉欢 <yinjihuan@fangjia.com>
2018-09-06 18:04:06 +0800

    eureka config update

--- monkey-eureka/src/main/java/com/github/eureka/MonkeyEurekaApplication.java
+++ monkey-eureka/src/main/java/com/github/eureka/MonkeyEurekaApplication.java
@@ -3,6 +3,11 @@ package com.github.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 /**
  * 注册中心启动入口
  * 
@@ -18,6 +23,18 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class MonkeyEurekaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MonkeyEurekaApplication.class, args);
+	}
+	
+	@Configuration
+	@EnableWebSecurity
+	public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	    @Override
+	    protected void configure(HttpSecurity http) throws Exception {
+	        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
+	        http.csrf().disable();
+	        // 支持httpBasic
+	        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+	    }
 	}
 }
 
